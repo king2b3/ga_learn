f8ed99df04f38bdc4f0ea16cd79ad7b4eca8bd72
Bayley King <king2b3@mail.uc.edu>
2022-02-26 11:56:19 -0500

    file structure created

--- p5/sketch.js
+++ p5/sketch.js
@@ -3,10 +3,6 @@ function setup() {
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
-  }
-  ellipse(mouseX, mouseY, 80, 80);
+  background(220);
+  ellipse(50,50,80,80);
 }
---
+++ src/es.py
@@ -0,0 +1,5 @@
+"""es.py
+Developer: Bayley King
+Date: 2-26-2022
+Descrition: Evolutionary Strategy framework
+"""
